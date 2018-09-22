717e566aa2c52dc59d21ca4fd29f86a1fa4a07dd
Sysconn <37973547+Sysconn@users.noreply.github.com>
2018-09-22 16:07:14 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'i8mV9dQ/J3QxDXf9dbo2JEOATJU+I7gewIcmRqHwqe2DP7JpGK8BJCe9xWSRBWhu/HCZk6FgBL8UC5gSwaTYf+8vKoKuiQKQ3eEf1EHn8R5SQBFrzUqg1QGbnO3oJu8Mwqt8ooRR/wMyqLl3mfARFQdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
